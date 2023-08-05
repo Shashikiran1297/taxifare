a256c5ca0d5a21e7c00b2fc090f447cfd504aea8
Shashikirana <126816648+Shashikiran1297@users.noreply.github.com>
2023-08-05 12:31:35 +0530

    Update taxifare.java
    adding program

--- taxifare.java
+++ taxifare.java
@@ -1,1 +1,31 @@
+package basicIfElse;
+
+import java.util.Scanner;
+
+public class taxiFare {
+	static int km;
+	public  void checkfare() {
+
+		if(km<=2) {
+			System.out.println("the fare is 25rs for "+ km +" kms");
+		}
+		else if(km<=7 ||km>2) {
+			System.out.println(25+(km-2)*20+" rs for "+km+"kms");
+		}
+		else if(km<=12 || km>7) {
+			System.out.println(125+(km-7)*25+"rs for "+km+"kms");
+		}
+		else if(km>12) {
+			System.out.println(225+(km-12)*35+"rs for "+km+"kms");
+		}
+		
+	}
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+    	System.out.println("plz enter your journey distance");
+    	km=s.nextInt();
+    	taxiFare tf=new taxiFare();
+    	tf.checkfare();	
+	}
+}
 
